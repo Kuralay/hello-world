e6d559c7a543b0ab1334980191f3524c54972390
Kuralay <kukniku@gmail.com>
2014-06-05 18:39:13 +0600

    Initial commit

---
+++ src/main/java/com/epam/ka/Runner.java
@@ -0,0 +1,10 @@
+package com.epam.ka;
+
+/**
+ * Created by Куралай on 05.06.2014.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
